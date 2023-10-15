d76b67dd5083fbc621aede49b5671006726bebf5
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-15 11:55:29 +0530

    Auto push

--- docdb.tf
+++ docdb.tf
@@ -4,4 +4,20 @@ resource "aws_docdb_cluster" "docdb" {
   master_username         = "admin1"
   master_password         = "roboshop1"
   skip_final_snapshot     = true
+
+  vpc_security_group_ids  =  
+  db_subnet_group_name    =  aws_db_subnet_group.docdb_subnet_group.name
 }
+
+# in which VPC , in which subnet it has to be created is not mentioned
+
+
+# creates subnet group
+resource "aws_db_subnet_group" "docdb_subnet_group" {
+  name                = "roboshop-docdb-${var.ENV}-subnetgroup"
+  subnet_ids          = [ data.terraform_remote_state.vpc.PRIVATE_SUBNET_IDS ]
+
+  tags = {
+    Name = "roboshop-docdb-${var.ENV}-subnetgroup"
+  }
+} 
