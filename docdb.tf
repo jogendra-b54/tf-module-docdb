35cdcea540caee814c4019978518423847af3a49
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-15 12:33:01 +0530

    Auto push

--- docdb.tf
+++ docdb.tf
@@ -6,7 +6,7 @@ resource "aws_docdb_cluster" "docdb" {
   skip_final_snapshot     = true
 
   vpc_security_group_ids  =  [aws_security_group.allows_docdb.id]
-  db_subnet_group_name    =  aws_db_subnet_group.docdb_subnet_group.name
+  db_subnet_group_name    =  aws_docdb_subnet_group.docdb_subnet_group.name
 }
 
 # in which VPC , in which subnet it has to be created is not mentioned
