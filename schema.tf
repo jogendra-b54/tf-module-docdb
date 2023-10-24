0f050a225fa1146e581602be1fc3f0732aebfc17
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-24 21:12:31 +0530

    Auto push

--- schema.tf
+++ schema.tf
@@ -11,6 +11,7 @@ resource "null_resource" "docdb_schema" {
              wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem 
              curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
              unzip -o /tmp/mongodb.zip
+             cd mongodb-main
              mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1 < catalogue.js
              mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password roboshop1 < user.js
         EOF
