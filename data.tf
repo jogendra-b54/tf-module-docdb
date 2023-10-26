d9fe4a5ba611cec8ad33cd24ca8af26c8b75cf25
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-26 17:14:29 +0530

    Auto push

--- data.tf
+++ data.tf
@@ -15,6 +15,11 @@ data "aws_secretsmanager_secret" "secrets" {
   name = "robot/secrets"
 }
 
-output "data" {
-  value = data.aws_secretsmanager_secret.secrets
+
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id     = data.aws_secretsmanager_secret.secrets.id 
 }
+
+# output "example" {
+#   value = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
+# }
