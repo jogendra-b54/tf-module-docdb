35622bacd777a379d7a94df922fb407ecc9b72c4
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-26 18:14:11 +0530

    Auto push

--- data.tf
+++ data.tf
@@ -11,14 +11,14 @@
  }
 
 
-data "aws_secretsmanager_secret" "secrets" {
-  name = "robot/secrets"
-}
+# data "aws_secretsmanager_secret" "secrets" {
+#   name = "robot/secrets"
+# }
 
 
-data "aws_secretsmanager_secret_version" "secret_version" {
-  secret_id     = data.aws_secretsmanager_secret.secrets.id 
-}
+# data "aws_secretsmanager_secret_version" "secret_version" {
+#   secret_id     = data.aws_secretsmanager_secret.secrets.id 
+# }
 
 # output "example" {
 #   value = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
--- docdb.tf
+++ docdb.tf
@@ -1,8 +1,8 @@
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "roboshop-${var.ENV}-docdb"
   engine                  = "docdb"
-  master_username         = locals.DOCDB_USER
-  master_password         = locals.DOCDB_PASS
+  master_username         = "admin1"
+  master_password         = "roboshop1"
   skip_final_snapshot     =  true
 
   vpc_security_group_ids  =  [aws_security_group.allows_docdb.id]
