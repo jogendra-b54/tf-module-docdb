a00cbec883ffa1fe5263f2ca686449e726fd2f61
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-26 18:15:13 +0530

    Auto push

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-locals {
-  DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
-  DOCDB_PASS = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_PASSWORD"]
-}
+# locals {
+#   DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
+#   DOCDB_PASS = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_PASSWORD"]
+# }
